55565e7f04a5155ae4054b25d0b8948126102a72
PascalAllekotte <pascalallekotte1@web.de>
2024-11-18 18:09:06 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Pascal Allekotte on 18.11.24.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
